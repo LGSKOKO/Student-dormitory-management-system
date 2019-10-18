746f6d1ac292d98794be7e05deea97a71715931b
LGSKOKO <420422844@qq.com>
2019-10-18 09:13:01 +0800

    超级管理员实体类

---
+++ dormitory_managment/src/main/java/com/lgs/entity/Super.java
@@ -0,0 +1,37 @@
+package com.lgs.entity;
+
+
+public class Super {
+
+  private long id;//主键
+  private String username;//主键 用户名/编号
+  private String password;//密码
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+}
