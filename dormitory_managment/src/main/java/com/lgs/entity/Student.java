807a2b275a2a4e870a77b2f80f219d709cb66b96
LGSKOKO <420422844@qq.com>
2019-10-23 23:22:25 +0800

    学生实体类

---
+++ dormitory_managment/src/main/java/com/lgs/entity/Student.java
@@ -0,0 +1,109 @@
+package com.lgs.entity;
+
+//tb_student表对应的实体类
+public class Student {
+
+  private String username;//主键 编号/账号
+  private String password;//密码
+  private String sex;//性别
+  private String buildingNo;//所依赖的外键 宿舍楼号
+  private String roomNo;//所依赖的外键 宿舍号
+  private String classNo;//所依赖的外键 班级号
+  private String collegeNo;//所依赖的外键 院系号
+  private Building building;//成员属性
+  private Room room;//成员属性
+  private Class classes;//成员属性
+  private College college;//成员属性
+
+
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
+  public String getSex() {
+    return sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+  public String getBuildingNo() {
+    return buildingNo;
+  }
+
+  public void setBuildingNo(String buildingNo) {
+    this.buildingNo = buildingNo;
+  }
+
+  public String getRoomNo() {
+    return roomNo;
+  }
+
+  public void setRoomNo(String roomNo) {
+    this.roomNo = roomNo;
+  }
+
+  public String getClassNo() {
+    return classNo;
+  }
+
+  public void setClassNo(String classNo) {
+    this.classNo = classNo;
+  }
+
+  public String getCollegeNo() {
+    return collegeNo;
+  }
+
+  public void setCollegeNo(String collegeNo) {
+    this.collegeNo = collegeNo;
+  }
+
+  public Building getBuilding() {
+    return building;
+  }
+
+  public void setBuilding(Building building) {
+    this.building = building;
+  }
+
+  public Room getRoom() {
+    return room;
+  }
+
+  public void setRoom(Room room) {
+    this.room = room;
+  }
+
+  public Class getClasses() {
+    return classes;
+  }
+
+  public void setClasses(Class classes) {
+    this.classes = classes;
+  }
+
+  public College getCollege() {
+    return college;
+  }
+
+  public void setCollege(College college) {
+    this.college = college;
+  }
+}
