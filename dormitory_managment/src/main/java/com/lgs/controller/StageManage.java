e89c084d54dae29e83b4094ee9e77f47f914f717
LGSKOKO <420422844@qq.com>
2019-11-01 08:38:27 +0800

    stage管理类 全局变量类

---
+++ dormitory_managment/src/main/java/com/lgs/controller/StageManage.java
@@ -0,0 +1,18 @@
+package com.lgs.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ：李先生
+ * @date ：Created in 2019/10/22 17:44
+ * @description：stage管理类 全局变量类
+ * @modified By：
+ * @version: $
+ */
+public class StageManage {
+    public static Map<String, Object> CONTROLLER=new HashMap<String, Object>();
+
+}
